a3b1f15148232321f3921504ec69fe914c6ebbdf
Maria Persico <m.persico@tigem.it>
2014-04-27 23:52:14 +0200

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+    create a square matrix that I know is invertible
+    invoke makeCacheMatrix() function passing it the matrix I created.  This returns a list of functions.
+    invoke cacheSolve() function passing it the list of functions returned.  This should calculate the matrix and save it in the cache.
+    invoke cacheSolve() function a second time passing to it the list of functions.  On this second invocation you should see the message.
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -5,11 +10,39 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }        
+        
+        
+        get <- function() x
+        setinvmat <- function(InvMat) im <<- InvMat
+        getinvmat <- function() im
+        list(set = set, get = get,
+             setinvmat = setinvmat,
+             getinvmat = getinvmat)
+
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=list(), ...) {
         ## Return a matrix that is the inverse of 'x'
+
+
+       invmat <- x$getinvmat()#from cache
+        if(!is.null(invmat)) {
+                message("getting cached data")
+                return(invmat)
+        }
+
+        mat <- x$get()
+         invmat<- solve(mat)
+         x$setinvmat(invmat)
+         invmat
+
+
 }
